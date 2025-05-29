eb48f4f8698972698394fc7fb85416a98ab1e9c4
ArpanSankesh <arpansankesh830@gmail.com>
2025-05-29 19:35:57 +0530

    ui: build Categories Section

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroBanner from '../components/HeroBanner'
+import Categories from '../components/Categories'
 
 const Home = () => {
   return (
     <div className='mt-10'>
         <HeroBanner/>
+        <Categories/>
     </div>
   )
 }
