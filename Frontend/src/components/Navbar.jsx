a3c27788c509772b0b76b26210ffef2f1bcb8778
ArpanSankesh <arpansankesh830@gmail.com>
2025-06-21 00:36:42 +0530

    Feature : Cart Icon Counter

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -6,23 +6,23 @@ import { useAppContext } from '../context/AppContext'
 const Navbar = () => {
 
     const [open, setOpen] = useState(false)
-    const {user, setUser, setShowUserLogin, navigate , setSearchQuery, searchQuery} = useAppContext()
+    const { user, setUser, setShowUserLogin, navigate, setSearchQuery, searchQuery, getCartCount } = useAppContext()
 
     const logout = () => {
         setUser(null)
         navigate('/')
     }
 
-    useEffect(()=>{
-        if(searchQuery.length > 0){
+    useEffect(() => {
+        if (searchQuery.length > 0) {
             navigate("/products")
         }
-}, [searchQuery])
+    }, [searchQuery])
 
     return (
         <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
 
-            <NavLink to={'/'} onClick={()=> setOpen(false)} className="flex gap-2">
+            <NavLink to={'/'} onClick={() => setOpen(false)} className="flex gap-2">
                 <img src="\public\favicon.svg" alt="" />
                 <h1 className="h-9 font-bold text-3xl">FreshCart</h1>
             </NavLink>
@@ -34,55 +34,62 @@ const Navbar = () => {
                 <NavLink to={"/Contact"}>Contact</NavLink>
 
                 <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
-                    <input onChange={(e)=>{setSearchQuery(e.target.value)}} className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search products" />
+                    <input onChange={(e) => { setSearchQuery(e.target.value) }} className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search products" />
                     <img src={assets.search_icon} alt="" />
                 </div>
 
-                <div onClick={() => {navigate('/cart')}} className="relative cursor-pointer">
+                <div onClick={() => { navigate('/cart') }} className="relative cursor-pointer">
                     <img src={assets.cart_icon} alt="cart" className='w-6 opacity' />
-                    <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">0</button>
+                    <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">{getCartCount()}</button>
                 </div>
 
-                {!user ? (<button onClick={() => {setShowUserLogin(true)}} className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
+                {!user ? (<button onClick={() => { setShowUserLogin(true) }} className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
                     Login
-                </button>): (
+                </button>) : (
                     <div className='relative group'>
-                <img src={assets.profile_icon} className='w-10' alt="" />
-                <ul className='hidden group-hover:block absolute top-10 right-0 bg-white shadow border border-gray-200 py-2.5 w-30 rounded-md text-sm z-40'>
-                    <li onClick={() => {navigate('my-orders')}} className='p-1.5 pl-3 hover:bg-primary-dull/10 cursor-pointer'>My Products</li>
-                    <li onClick={logout} className='p-1.5 pl-3 hover:bg-primary-dull/10 cursor-pointer'>Logout</li>
-                </ul>
+                        <img src={assets.profile_icon} className='w-10' alt="" />
+                        <ul className='hidden group-hover:block absolute top-10 right-0 bg-white shadow border border-gray-200 py-2.5 w-30 rounded-md text-sm z-40'>
+                            <li onClick={() => { navigate('my-orders') }} className='p-1.5 pl-3 hover:bg-primary-dull/10 cursor-pointer'>My Products</li>
+                            <li onClick={logout} className='p-1.5 pl-3 hover:bg-primary-dull/10 cursor-pointer'>Logout</li>
+                        </ul>
                     </div>
                 )}
             </div>
 
-            <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" className="sm:hidden">
-                {/* Menu Icon SVG */}
-                <img src={assets.menu_icon} alt="menu" />
-            </button>
+            <div className='flex items-center gap-6 sm:hidden'>
+                <div onClick={() => { navigate('/cart') }} className="relative cursor-pointer">
+                    <img src={assets.cart_icon} alt="cart" className='w-6 opacity' />
+                    <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">{getCartCount()}</button>
+                </div>
+
+                <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" >
+                    {/* Menu Icon SVG */}
+                    <img src={assets.menu_icon} alt="menu" />
+                </button>
+            </div>
 
             {/* Mobile Menu */}
             {open && (
-            <div className={`${open ? 'flex' : 'hidden'} absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}>
-                <NavLink to={"/home"} onClick={() => setOpen(false)}>Home</NavLink>
-                <NavLink to={"/products"} onClick={() => setOpen(false)}>All Products</NavLink>
-                {user &&
-                    <NavLink to={"/Contact"} onClick={() => setOpen(false)}>My Products</NavLink>
-                }
-                {!user ? (
-                    <button onClick={() => {
-                        setOpen(false);
-                        setShowUserLogin(true);
-                    }} className="cursor-pointer px-6 py-2 mt-2 bg-primary hover:bg-primary-dull transition text-white rounded-full text-sm">
-                        Login
-                    </button>
-                ) : (
-                    <button onClick={logout} className="cursor-pointer px-6 py-2 mt-2 bg-primary hover:bg-primary-dull transition text-white rounded-full text-sm">
-                        Logout
-                    </button>
-                )}
+                <div className={`${open ? 'flex' : 'hidden'} absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}>
+                    <NavLink to={"/home"} onClick={() => setOpen(false)}>Home</NavLink>
+                    <NavLink to={"/products"} onClick={() => setOpen(false)}>All Products</NavLink>
+                    {user &&
+                        <NavLink to={"/Contact"} onClick={() => setOpen(false)}>My Products</NavLink>
+                    }
+                    {!user ? (
+                        <button onClick={() => {
+                            setOpen(false);
+                            setShowUserLogin(true);
+                        }} className="cursor-pointer px-6 py-2 mt-2 bg-primary hover:bg-primary-dull transition text-white rounded-full text-sm">
+                            Login
+                        </button>
+                    ) : (
+                        <button onClick={logout} className="cursor-pointer px-6 py-2 mt-2 bg-primary hover:bg-primary-dull transition text-white rounded-full text-sm">
+                            Logout
+                        </button>
+                    )}
 
-            </div>
+                </div>
             )}
 
         </nav>
--- Frontend/src/context/AppContext.jsx
+++ Frontend/src/context/AppContext.jsx
@@ -16,7 +16,7 @@ export const AppContextProvider = ({ children }) => {
     const [products, setProducts] = useState([]);
     const [cartItems, setCartItems] = useState({});
     const [searchQuery, setSearchQuery] = useState({});
-    
+
 
     const fetchProducts = async () => {
         setProducts(dummyProducts)
@@ -56,11 +56,34 @@ export const AppContextProvider = ({ children }) => {
         setCartItems(cartData);
     }
 
+    // Get Cart Item Count
+
+    const getCartCount = () => {
+        let totalCount = 0;
+        for (const item in cartItems) {
+            totalCount += cartItems[item];
+        }
+        return totalCount;
+    }
+
+    const getCartAmount = () => {
+        let totalAmount = 0;
+        for (const items in cartItems) {
+            let itemInfo = products.find((product) => product._id == items);
+            if (cartItems[items] > 0) {
+                totalAmount += itemInfo.offerPrice * cartItems[items];
+            }
+        }
+        return Math.floor(totalAmount * 100) / 100;
+    }
+
     useEffect(() => {
         fetchProducts();
     }, [])
 
-    const value = { navigate, user, setUser, isSeller, setIsSeller, showUserLogin, setShowUserLogin, products, currency, addToCart, updateCartItems, removeFromCart, cartItems, searchQuery, setSearchQuery };
+    const value = { navigate, user, setUser, isSeller, setIsSeller, showUserLogin, setShowUserLogin, products, currency, addToCart, updateCartItems, 
+        removeFromCart, cartItems, searchQuery, setSearchQuery, getCartAmount, getCartCount
+     };
     return <AppContext.Provider value={value}>
         {children}
     </AppContext.Provider>
