96304cca172d026dc07f497f6242aedfd4e26ee1
ArpanSankesh <arpansankesh830@gmail.com>
2025-07-11 00:05:20 +0530

    Cart Realtime Update

--- Backend/controllers/addressController.js
+++ Backend/controllers/addressController.js
@@ -3,8 +3,9 @@ import Address from "../models/address.js"
 // Add Address : /api/address/add
 export const addAddress = async (req, res) => {
     try {
-    const { address, userId } = req.body;
-    await Address.create({...address, userId: userId});
+    const { address } = req.body;
+    const userId = req.userId;
+    await Address.create({...address, userId});
     res.json({success:true, message : "Address Added Successfully"})
     } catch (error) {
         console.log(error.message); 
@@ -15,9 +16,9 @@ export const addAddress = async (req, res) => {
 // Get Address : /api/address/get
 export const getAddress = async (req, res) => {
     try {
-    const { userId } = req.userId;
+    const userId = req.userId; 
     const addresses = await Address.find({userId})
-    res.json({success:true, message : addresses})
+    res.json({success:true, addresses})
     } catch (error) {
         res.json({success:false, message : error.message})
     }
